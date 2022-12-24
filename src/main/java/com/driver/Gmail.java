031084188778274a39cf864a080ddcd73a518988
Nandan <nk1399a@gmail.com>
2022-12-24 20:40:34 +0530

    new object approach

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -2,21 +2,21 @@ package com.driver;
 
 import java.util.*;
 
-import static java.util.Map.entry;
+
 
 public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
-    Map<Date,String> InboxMails;
-    Map<Date,String> TrashMail;
+    ArrayList<Mail> InboxMails;
+    ArrayList<Mail> TrashMail;
     int noOfMailsReceived;
     public Gmail(String emailId, int inboxCapacity) {
         super(emailId);
         this.inboxCapacity =inboxCapacity;
-        this.InboxMails = new HashMap<>();
-        this.TrashMail = new HashMap<>();
+        this.InboxMails = new ArrayList<>();
+        this.TrashMail = new ArrayList<>();
         this.noOfMailsReceived = 0;
     }
 
@@ -26,56 +26,58 @@ public class Gmail extends Email {
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
 
+        Mail newMail = new Mail(date,sender,message);
 
-
-
-
-        if(noOfMailsReceived==inboxCapacity){
+        if(InboxMails.size()==inboxCapacity){
 
 //            Date oldestMail = InboxMails.entrySet().stream().findFirst().get().getKey();
 //            Date oldestMail = InboxMails.entrySet().stream().reduce((one, two)->two).get().getKey();
 //
 //            InboxMails.entrySet().removeIf(entry -> (oldestMail == entry.getKey()));
+            Mail oldestMail = InboxMails.get(0);
+            InboxMails.remove(oldestMail);
+            TrashMail.add(oldestMail);
+            InboxMails.add(newMail);
 
-            Date oldestMail;
-            List<Date> ls = new ArrayList<>(InboxMails.keySet());
-            if(!ls.isEmpty()){
-                oldestMail=ls.get(0);
-                InboxMails.entrySet().remove(oldestMail);
-            }
+//            List<Date> ls = new ArrayList<>(InboxMails.keySet());
+//            if(!ls.isEmpty()){
+//                oldestMail=ls.get(0);
+//                InboxMails.entrySet().remove(oldestMail);
+//            }
 
+        }else{
+            InboxMails.add(newMail);
         }
-        InboxMails.put(date,message);
-        noOfMailsReceived++;
+
+
     }
 
     public void deleteMail(String message){
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
 
-        Iterator<Map.Entry<Date,String>> iterator = InboxMails.entrySet().iterator();
+        ListIterator<Mail> iterator = InboxMails.listIterator();
         while(iterator.hasNext()){
-            Map.Entry<Date,String> entry = iterator.next();
-            if(message.equals(entry.getValue())){
-                TrashMail.put(entry.getKey(),message);
+            if(message.equals(iterator.next().getMessage())){
                 iterator.remove();
             }
         }
-
 
     }
 
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
-        if(noOfMailsReceived==0){
+        if(InboxMails.size()==0){
             return null;
         }else{
 //            List<Date> listKeys = new ArrayList<>(InboxMails.keySet());
 //            String latestMessage = InboxMails.get(listKeys.get(listKeys.size()-1));
 //              String latestMessage = InboxMails.entrySet().stream().reduce((one, two)->two).get().getValue();
-              String latestMessage = InboxMails.entrySet().stream().findFirst().get().getValue();
-              return latestMessage;
+//              String latestMessage = InboxMails.entrySet().stream().findFirst().get().getValue();
+
+            String latestMessage = InboxMails.get(InboxMails.size()-1).getMessage();
+            return latestMessage;
         }
 
 
@@ -84,10 +86,12 @@ public class Gmail extends Email {
     public String findOldestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
-        if(noOfMailsReceived==0){
+        if(InboxMails.size()==0){
             return null;
         }else{
-            String oldestMessage = InboxMails.entrySet().stream().reduce((one, two)->two).get().getValue();
+//            String oldestMessage = InboxMails.entrySet().stream().reduce((one, two)->two).get().getValue();
+
+            String oldestMessage = InboxMails.get(0).getMessage();
             return oldestMessage;
         }
 
@@ -98,32 +102,32 @@ public class Gmail extends Email {
         //It is guaranteed that start date <= end date
         int startIdx=0;
         int endIdx=0;
-        List<Date> list = new ArrayList<>(InboxMails.keySet());
-        ListIterator<Date> iterator = list.listIterator();
+
+        ListIterator<Mail> iterator = InboxMails.listIterator();
         while(iterator.hasNext()){
-            Date date = iterator.next();
-            if(date.equals(start)){
+            Date currentMailDate = iterator.next().getDate();
+            if(currentMailDate.equals(start)){
                 startIdx=iterator.previousIndex();
                 break;
             }
         }
-        ListIterator<Date> iterate = list.listIterator(startIdx);
+        ListIterator<Mail> iterate = InboxMails.listIterator(startIdx);
         while(iterator.hasNext()){
-            Date date = iterator.next();
-            if(date.equals(end)){
+            Date currentMailDate = iterator.next().getDate();
+            if(currentMailDate.equals(end)){
                 continue;
             }else{
                 endIdx= iterate.previousIndex();
             }
         }
-        int mailBetween = endIdx-startIdx+1;
-        return mailBetween;
+        int totalmailBetween = endIdx-startIdx+1;
+        return totalmailBetween;
 
     }
 
     public int getInboxSize(){
         // Return number of mails in inbox
-        return noOfMailsReceived-1;
+        return InboxMails.size();
 
     }
 
