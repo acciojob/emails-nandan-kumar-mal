8d772a322da6dc1b010fd5c1af83c4df7d44a12b
Nandan <nk1399a@gmail.com>
2022-12-24 19:44:22 +0530

    changes in meeting

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -2,6 +2,7 @@ package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.sql.SQLOutput;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,7 +30,7 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        int maxMeetingPossible=1;
+        int maxMeetingPossible=0;
         Collections.sort(calendar, new Comparator<Meeting>() {
             @Override
             public int compare(Meeting m1, Meeting m2) {
@@ -37,11 +38,15 @@ public class Workspace extends Gmail{
             }
         });
 
+//        for(Meeting m : calendar){
+//            System.out.println(m.getStartTime()+" "+m.getEndTime());
+//        }
+//        System.out.println();
+
         LocalTime et = calendar.get(0).getEndTime();
         for(int idx =1;idx< calendar.size();idx++){
-            if(calendar.get(idx).getStartTime().compareTo(et) < 0 ){
+            if(calendar.get(idx).getStartTime().compareTo(et) > 0 ){
                 et = calendar.get(idx).getEndTime();
-                continue;
             }else{
                 et = calendar.get(idx).getEndTime();
                 maxMeetingPossible++;
